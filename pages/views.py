9d5e2296d1df4e5cbbbb891fdca2f191aaa98f91
Anurag Singhal <anurag7noni@gmail.com>
2020-02-17 14:59:00 -0800

    Vendor Apply page commit one

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request,*args, **kwargs):
 	return render(request,"vendor_landing.html",{})
+
+def vendor_apply_view(request,*args, **kwargs):
+	return render(request,"vendor_apply.html",{})
