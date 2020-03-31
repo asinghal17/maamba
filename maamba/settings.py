606158fec7f0b8758e77ed1fbf905ed06328cc5e
Anurag Singhal <anurag7noni@gmail.com>
2020-03-30 18:29:41 -0700

    Adding DB Services

--- maamba/settings.py
+++ maamba/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import cloudinary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'phonenumber_field',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Service(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	service_name = models.CharField(max_length=30)
+	service_description = models.CharField(max_length=30)
+	cover_img = models.CharField(max_length=200)
+	application_link = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.service_name
+
+
+# class Vendor(models.Model):
+# 	company_name = models.CharField(max_length=200)
+# 	first_name = models.CharField(max_length=30)
+# 	last_name = models.CharField(max_length=30)
+# 	phone_number= PhoneNumberField(blank=True)
+# 	primary_location = models.CharField(max_length=30)
+# 	website_url = models.CharField(max_length=200)
+# 	instagram = models.CharField(max_length=20)
+# 	description = models.TextField()
+# 	service_id = models.ForeignKey(Service, on_delete=models.SET_NULL,null=True)
+# 	created_at = models.DateTimeField(auto_now_add=True)
+# 	updated_at = models.DateTimeField(auto_now=True)
+
+# 	def __str__(self):
+# 		return self.company_name
