5e8a9a3f2aff892475f7b052eb6962e065346bf4
Anurag Singhal <anurag7noni@gmail.com>
2020-04-03 17:05:40 -0700

    Adding rich text editing for Blog

--- maamba/settings.py
+++ maamba/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'blog',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -128,8 +129,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
