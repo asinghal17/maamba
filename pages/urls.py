7e81fd304e12d7fc00cb18fd5b531da07826a796
Anurag Singhal <anurag7noni@gmail.com>
2020-04-06 17:41:41 -0700

    Adding shell for profile

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('vendor-search/',vendor_search_landing_view, name='vendor-search'),
     path('search/',vendor_search_results_view, name='vendor-search-results'),
     path('vendor-apply/',vendor_apply_view, name='vendor-apply'),
-    # path("vendor/<slug:slug>",VendorDetailView.as_view(), name='vendor-detail'),
+    path("vendor/<slug:slug>",VendorDetailView.as_view(), name='vendor-detail'),
 ]
