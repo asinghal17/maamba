f087d8d6fe9fdaf4ed0a10267d0bb1704e40b8e1
Anurag Singhal <anurag7noni@gmail.com>
2020-04-12 17:22:37 -0700

    Changing to updated at, modify url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     # path("post/<int:pk>",post_detail_view, name='post-detail'),
     path("blog/",PostListView.as_view(), name='blog'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
